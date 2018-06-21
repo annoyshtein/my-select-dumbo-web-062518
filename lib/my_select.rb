b0f7e4f1401c060b86c73dba8639a98022f302bd
Alex Neustein <alex.neustein@gmail.com>
2018-06-21 19:44:16 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
     new_collection = []
     while i < collection.length
       variable = yield collection[i]
-      new_collection << variable
+      new_collection << variable if variable == true
       i = i + 1
     end
     new_collection
